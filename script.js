9c5aa443fc6782906700970f9c47bb08df53e938
Matheus <matheus.ryan@discente.ufma.br>
2025-09-05 16:26:22 -0300

    feat: add initScrollAnimation function

--- script.js
+++ script.js
@@ -67,3 +67,28 @@ linksInterno.forEach((link) => {
 }
 
 initScroll();
+
+
+function initScrollAnimation() {
+
+const sections = document.querySelectorAll('.js-scroll');
+
+const windowHalf = window.innerHeight * 0.6;
+
+function scrollAnimation() {
+    sections.forEach((section) => {
+        
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - windowHalf) < 0;
+        if(isSectionVisible) 
+            section.classList.add('ativo');
+         else {
+            section.classList.remove('ativo');
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollAnimation);
+};
+
+initScrollAnimation();
