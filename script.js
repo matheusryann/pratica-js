abd78edbd0e46f9bb277be8efe00055685836012
Matheus <matheus.ryan@discente.ufma.br>
2025-09-05 15:24:32 -0300

    feat: add accordion list faq and its css styles

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+
+function initTabNav() {
 const tabMenu = document.querySelectorAll('.js-tabmenu li');
 
 const tabContent = document.querySelectorAll('.js-tabcontent section');
 
+if(tabMenu.length && tabContent.length) {
+
+tabContent[0].classList.add('ativo');
 
 function activeTab(index) {
     tabContent.forEach((section) => {
@@ -15,4 +20,22 @@ tabMenu.forEach((item, index) => {
         activeTab(index);
     });
 });
+}
+}
+
+initTabNav();
+
+const accordionList = document.querySelectorAll('.js-accordion dt');
+
+accordionList[0].classList.add('ativo');
+accordionList[0].nextElementSibling.classList.add('ativo');
+
+function activeAccordion() {
+    this.classList.toggle('ativo');
+    this.nextElementSibling.classList.toggle('ativo');
+}
+
+accordionList.forEach((item) => {
+    item.addEventListener('click', activeAccordion);
+})
 
