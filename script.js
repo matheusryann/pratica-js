561633f3dbeb3e57e2c0a5cbe5f68c427395b0bb
Matheus <matheus.ryan@discente.ufma.br>
2025-09-05 14:41:24 -0300

    feat: tab navigation

---
+++ script.js
@@ -0,0 +1,18 @@
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+
+function activeTab(index) {
+    tabContent.forEach((section) => {
+        section.classList.remove('ativo');
+    });
+    tabContent[index].classList.add('ativo');
+}
+
+tabMenu.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        activeTab(index);
+    });
+});
+
