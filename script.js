beb18bc9247772184c2f10ac5aa513148ba0072f
Matheus <matheus.ryan@discente.ufma.br>
2025-09-05 16:11:02 -0300

    feat: add scroll function

--- script.js
+++ script.js
@@ -25,8 +25,10 @@ tabMenu.forEach((item, index) => {
 
 initTabNav();
 
-const accordionList = document.querySelectorAll('.js-accordion dt');
 
+function initAccordionList() {
+const accordionList = document.querySelectorAll('.js-accordion dt');
+if(accordionList.length) {
 accordionList[0].classList.add('ativo');
 accordionList[0].nextElementSibling.classList.add('ativo');
 
@@ -37,5 +39,31 @@ function activeAccordion() {
 
 accordionList.forEach((item) => {
     item.addEventListener('click', activeAccordion);
-})
+});  
+}
+}
 
+initAccordionList();
+
+
+function initScroll() {
+const linksInterno = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    
+    section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+    });
+
+};
+
+linksInterno.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+});
+}
+
+initScroll();
